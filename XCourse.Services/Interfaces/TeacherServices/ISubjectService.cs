b13e8a09fe7fe48265b12b25a386ff5c17a469ab
Zahran <abdelrahmanramadan416@gmail.com>
2025-04-08 09:24:50 +0200

    Adding service classes for the teacher subjects.

---
+++ XCourse.Services/Interfaces/TeacherServices/ISubjectService.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using XCourse.Core.DTOs.Teachers;
+using XCourse.Core.Entities;
+
+namespace XCourse.Services.Interfaces.TeacherServices
+{
+    public interface ISubjectService
+    {
+        public Task<IEnumerable<Subject>> GetSubjectsNotTaughtByTeacherAsync(int teacherId, int? take, int? skip);
+        Task<IEnumerable<Subject>> GetSubjectsPerTeacher(int teacherId, int? take, int? skip);
+        public Task<ResponseSubjectDto> AssignSubject(int subjectId, int teacherId);
+        public Task<ResponseSubjectDto> RemoveSubject(int subjectId, int teacherId);
+    }
+}
