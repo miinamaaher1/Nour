be304372e7d0ddd7ff69683c85421faa2a51e58e
miinamaaher1 <minamaher1515@gmail.com>
2025-04-13 01:43:02 +0200

    added styles to center, my groups, group details, group preview views

--- XCourse.Core/ViewModels/StudentsViewModels/GroupDetails.cs
+++ XCourse.Core/ViewModels/StudentsViewModels/GroupDetails.cs
@@ -13,12 +13,12 @@ namespace XCourse.Core.ViewModels.StudentsViewModels
         public string Key { set; get; }
         public virtual Room? DefaultRoom { get; set; }
         public virtual ICollection<Session>? Sessions { get; set; }
-        public WeekDay DefaultSessionDays { get; set; }
-        public bool IsActive { get; set; } = true;
+        public List<DefaultTimeVM> Defaults { get; set; }
         public bool IsPrivate { get; set; }
         public bool IsOnline { get; set; }
         public Teacher Teacher { get; set; }
         public byte[]? CoverPicture { get; set; }
-        public String Subject { get; set; }
+        public Subject Subject { get; set; }
+        public decimal PricePerSession { get; set; }
     }
 }
