4202f63466372a97055fc2e4d45ac1a435367812
AhmedYasserMohammed <ahmedyasser200220@gmail.com>
2025-04-18 01:46:25 +0200

    Added PendingRequests in Assistant's Area

---
+++ XCourse.Core/ViewModels/AssistantViewModels/PendingRequestVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCourse.Core.Entities;
+
+namespace XCourse.Core.ViewModels.AssistantViewModels
+{
+    public class PendingRequestVM
+    {
+        public AssistantInvitationStatus Status { get; set; }
+    }
+}
---
+++ XCourse.Services/Interfaces/AssistantServices/IPendingRequestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using XCourse.Core.ViewModels.AssistantViewModels;
+
+namespace XCourse.Services.Interfaces.AssistantServices
+{
+    public interface IPendingRequestService
+    {
+        Task<ICollection<PendingRequestVM>> GetPendingRequestsAsync(ClaimsPrincipal user);
+    }
+}
