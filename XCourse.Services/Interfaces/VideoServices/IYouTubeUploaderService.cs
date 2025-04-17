d1d5eaa378523b23b29c3ca8c2d789acd4414f69
miinamaaher1 <minamaher1515@gmail.com>
2025-04-18 01:25:58 +0200

    implemented the layout for the manage account pages, and added barnding logos and favicon, added youtube service

---
+++ XCourse.Services/Interfaces/VideoServices/IYouTubeUploaderService.cs
@@ -0,0 +1,7 @@
+﻿namespace XCourse.Services.Implementations.VideoServices
+{
+    public interface IYouTubeUploaderService
+    {
+        Task<string> UploadVideoAsync(Stream videoStream, string title, string description);
+    }
+}
