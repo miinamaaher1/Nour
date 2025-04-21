d8535ee1ac129b34df1a67d8fabf71217897c1b1
Shrouk Elmohalhel <Shrouk Elmohalhel@DESKTOP-OG0DKR8>
2025-04-22 00:12:55 +0200

    added centeradmin Dashboard

---
+++ XCourse.Services/Interfaces/CenterAdminServices/ICenterAdminHomeService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XCourse.Core.ViewModels.CenterAdminViewModels;
+using XCourse.Core.ViewModels.StudentsViewModels;
+
+namespace XCourse.Services.Interfaces.CenterAdminServices
+{
+    public interface ICenterAdminHomeService
+    {
+
+        public  Task<List<RoomHomeViewModel>> GetAllRooms(string id);
+
+        Task<List<TopCenterViewModel>> GetTopCenter(string id);
+    }
+}
