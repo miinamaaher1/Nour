b6a7d644328349c40305d61ac318e633a0ea79bf
Zahran <abdelrahmanramadan416@gmail.com>
2025-04-21 17:33:10 +0200

    Adding Assistant Home Dashboard & Added Assistant Layout & Restyling  the Groups View For Assistant

--- XCourse.Web/ServicesCollections/AssistantServiceCollectionExtensions.cs
+++ XCourse.Web/ServicesCollections/AssistantServiceCollectionExtensions.cs
@@ -11,6 +11,7 @@ namespace XCourse.Web.ServicesCollections
             services.AddScoped<IPendingRequestService, PendingRequestService>();
             services.AddScoped<IGroupService, GroupService>();
             services.AddScoped<IGroupDetailsService, GroupDetailsService>();
+            services.AddScoped<IHomeService, HomeService>();
 
             return services;
         }
